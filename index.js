03edfc94af78df1265dd5c298a3314aff7449c4e
Zen-Coderon <gaganreddyedu@gmail.com>
2025-07-06 20:50:29 +0530

    backend trace4 added

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ const PORT = process.env.PORT || 5000;
 
 // Allow CORS for Vercel frontend and localhost
 app.use(cors({
-  origin: [
-    'https://medi-fundr.vercel.app',
-    'http://localhost:5000',
-    'http://localhost:3000'
-  ],
+  origin: function (origin, callback) {
+    const allowed = [
+      'https://medi-fundr.vercel.app',
+      'http://localhost:5000',
+      'http://localhost:3000',
+      undefined // allow Postman, curl, or direct server-to-server
+    ];
+    if (allowed.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST'],
   credentials: true
 }));
