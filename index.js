0c8ccfdf7613f131b2098e868351f81ade7ceb78
Zen-Coderon <gaganreddyedu@gmail.com>
2025-07-06 10:34:35 +0530

    backend trace3 added

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const cors = require('cors');
 const loanRoutes = require('./routes/loan');
 require('dotenv').config();
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// Allow CORS for Vercel frontend and localhost
+app.use(cors({
+  origin: [
+    'https://medi-fundr.vercel.app',
+    'http://localhost:5000',
+    'http://localhost:3000'
+  ],
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 app.use(express.json());
 const path = require('path');
 
